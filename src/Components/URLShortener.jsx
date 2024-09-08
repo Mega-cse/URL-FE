81b59c7ffc095e70ce51880d2ead0f7da032b8ea
Megavardhini.P <megavardhini1958@gmail.com>
2024-09-08 23:08:18 +0530

    Update URLShortener.jsx

--- src/Components/URLShortener.jsx
+++ src/Components/URLShortener.jsx
@@ -14,7 +14,7 @@ const URLShortener = () => {
         // Check if the user is authenticated
         const token = localStorage.getItem('token');
         if (!token) {
-            navigate('/login');
+            navigate('/');
         }
     }, [navigate]);
 
